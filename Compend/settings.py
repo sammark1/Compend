a4b9676980511c2af2a3de602726b3b3dc728457
Sammark1 <samaustinmark@gmail.com>
2022-05-04 15:06:44 -0400

    added models NPC

--- Compend/settings.py
+++ Compend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,8 @@ WSGI_APPLICATION = 'Compend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'compend',
     }
 }
 
@@ -117,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,95 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Campaign(models.Model):
+    name = models.CharField(max_length = 32)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+ALIGNMENT_CHOICES = (
+    ("LG", "Lawful Good"),
+    ("LN", "Lawful Neutral"),
+    ("LE", "Lawful Evil"),
+    ("NG", "Neutral Good"),
+    ("TN", "True Neutral"),
+    ("NE", "Neutral Evil"),
+    ("CG", "Chaotic Good"),
+    ("CN", "Chaotic Neutral"),
+    ("CE", "Chaotic Evil"),
+)
+
+PRONOUN_CHOICES = (
+    ("they", "They/Them"),
+    ("she", "She/Her"),
+    ("he", "he/him"),
+)
+
+CLASS_CHOICES = (
+    ("Commoner"),
+    ("Barbarian"),
+    ("Bard"),
+    ("Cleric"),
+    ("Druid"),
+    ("Fighter"),
+    ("Monk"),
+    ("Paladin"),
+    ("Ranger"),
+    ("Rogue"),
+    ("Sorcerer"),
+    ("Warlock"),
+    ("Wizard"),
+    ("Artificer"),
+    ("Blood Hunter"),
+)
+
+RACE_CHOICES = (
+    ("Dragonborn"),
+    ("Dwarf"),
+    ("Elf"),
+    ("Gnome"),
+    ("Half-Elf"),
+    ("Halfling"),
+    ("Half-Orc"),
+    ("Human"),
+    ("Teifling"),
+    ("Aarakocra"),
+    ("Genasi"),
+    ("Goliath"),
+    ("Aasimar"),
+    ("Bugbear"),
+    ("Firbolg"),
+    ("Goblin"),
+    ("Hobgoblin"),
+    ("Kenku"),
+    ("Kobold"),
+    ("Lizardfolk"),
+    ("Orc"),
+    ("Tabaxi"),
+    ("Triton"),
+    ("Yuan-ti Pureblood"),
+    ("Tortle"),
+    ("Gith"),
+)
+
+class NPC(models.Model):
+    given_name = models.CharField(max_length = 32)
+    family_name = models.CharField(max_length = 32 blank=True)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    alignmnet = models.CharField(choices=ALIGNMENT_CHOICES)
+    pronoun = models.CharField(choices = PRONOUN_CHOICES)
+    npc_class = models.CharField(choices = CLASS_CHOICES, default='Commoner')
+    npc_race = models.CharField(choices = RACE_CHOICES)
+    age = models.IntegerField(default=20)
+    physical = models.CharField(max_length=1024, blank=True)
+    profession = models.CharField(max_length=64, blank=True)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.given_name} {self.family_name}"
+
+    class Meta:
+        ordering = ['designation']
